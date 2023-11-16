a4e8c383da2ce334a38b8d6be472aacc4c1dd771
mdjawwad <mdjawwad790@gmail.com>
2023-11-16 15:25:20 +0530

    Error Handling utils added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import mongodbConnection from "./db/db.js";
 import dotenv from "dotenv";
-
+import express from "express";
 dotenv.config({ path: "./env" });
-
-mongodbConnection();
+const app = express()
+mongodbConnection()
+  .then(()=>{
+    app.listen(process.env.PORT || 8000 ,()=>{
+        console.log(`Server is running at : ${process.env.PORT}`);
+    })
+  })
+  .catch((error) => {
+    console.log("mongodb connention failed ;(", error);
+  });
